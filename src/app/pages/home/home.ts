6bc6fda46d5f1f5e8435b22f20f9d93a0d810c36
Reddy <nagendharreddy22@gmail.com>
2025-11-23 11:24:43 +0530

    👌 IMPROVE: Added home component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+import { PersonalInfo } from '../../models/portfolio.models';
+import { Data } from '../../services/data';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+   personalInfo: PersonalInfo | null = null;
 
+  constructor(private dataService: Data) {}
+
+  ngOnInit(): void {
+    this.dataService.getPortfolioData().subscribe(data => {
+      if (data) {
+        this.personalInfo = data.personal;
+      }
+    });
+  }
 }
