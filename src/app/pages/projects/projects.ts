ddcd77797b9ee36a5e6f2ab97d988af308a6c4d2
Reddy <nagendharreddy22@gmail.com>
2025-11-23 12:23:37 +0530

    👌 IMPROVE: Added projects component

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { Project } from '../../models/portfolio.models';
+import { Data } from '../../services/data';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule
+],
   templateUrl: './projects.html',
-  styleUrl: './projects.scss',
+  styleUrls: ['./projects.scss'],
 })
-export class Projects {
-
-}
+export class Projects implements OnInit {
+  projects: Project[] = [];
+  constructor(private dataService: Data) {}
+  ngOnInit(): void {
+    this.dataService.getPortfolioData().subscribe(data => {
+      if (data) {
+        this.projects = data.projects;
+      }
+    });
+  }
+ }
